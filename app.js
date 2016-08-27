bafe0ad60ee7a283b8d7fe9bbac208d9813cd6dc
Drew <deeringa@gmail.com>
2016-08-27 00:35:50 -0500

    adding api controllers

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
+var app = express();
+
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.NODE_PORT || 3000;
 
@@ -11,6 +13,8 @@ app.use('/assets', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString("mongo"));
+
 setupController(app);
+apiController(app);
 
 app.listen(port);
